111d24fe9ccd51606d965272bc0fb12e58ba2034
wyqzl <wuyuqian@admindeiMac-35.local>
2016-06-22 18:22:54 +0800

    iOS计算器
    编译出简易计算器界面

--- iOScalculator/ViewController.swift
+++ iOScalculator/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var x: UITextField!
+    @IBAction func one(sender: AnyObject) {
+    }
+    @IBAction func two(sender: AnyObject) {
+    }
+    @IBAction func three(sender: AnyObject) {
+    }
+    @IBAction func four(sender: AnyObject) {
+    }
+    @IBAction func five(sender: AnyObject) {
+    }
+    @IBAction func six(sender: AnyObject) {
+    }
+    @IBAction func seven(sender: AnyObject) {
+    }
+    @IBAction func eight(sender: AnyObject) {
+    }
+    @IBAction func nine(sender: AnyObject) {
+    }
+    @IBAction func sum(sender: AnyObject) {
+    }
+    @IBAction func jian(sender: AnyObject) {
+    }
+    @IBAction func cheng(sender: AnyObject) {
+    }
+    @IBAction func c(sender: AnyObject) {
+    }
+    @IBAction func dengyu(sender: AnyObject) {
+    }
+    @IBAction func dot(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
